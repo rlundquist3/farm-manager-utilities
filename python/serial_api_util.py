c5df799e9e01c48ef04d9225be0080fa85370f37
Riley Lundquist <rlundquist3@gmail.com>
2017-01-12 13:56:16 -0800

    removed duplicate files/cleaned up python

---
+++ python/serial_api_util.py
@@ -0,0 +1,31 @@
+import serial, requests
+
+serial = serial.Serial('/dev/cu.usbmodemFA141', 9600)
+
+url = 'http://192.168.2.4:3000/api/'
+cred = {
+    'username' : 'system',
+    'password' : 'cricket'
+    }
+
+# res = requests.post(url + 'login', data=cred)
+# print res.headers
+# print res.url
+# print res.json()
+# headers = {
+#     'X-Auth-Token' : res.json()['data']['authToken'],
+#     'X-User-Id' : res.json()['data']['userId']
+#     }
+
+while True:
+    str = serial.readline()
+    print str
+    data = {
+        'temp' : str.split(',')[1].strip(),
+        'humidity' : str.split(',')[0].strip()
+        }
+    print data
+
+    # res = requests.post(url + 'climate_data', data=data)
+    # print res.url
+    # print res
